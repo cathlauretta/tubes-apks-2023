5d240af944bd5ac30701577577850da88b5bb080
Ivan Aldy Ganesen <18221045@std.stei.itb.ac.id>
2023-11-10 18:30:11 +0700

    Penambahan Monitoring Tools
    Menggunakan tools Prometheus + Grafana -> Memasukannya ke repo

--- up.sh
+++ up.sh
@@ -1,1 +1,4 @@
+# sumber : https://www.linkedin.com/pulse/running-grafana-prometheus-docker-stephen-townshend/
+docker run --rm --name my-prometheus --network prometheus_grafana --network-alias prometheus --publish 9090:9090 --volume ${pwd}\prometheus.yml:/etc/prometheus/prometheus.yml --detach prom/prometheus
+docker run --rm --name grafana --network prometheus_grafana --network-alias grafana --publish 3000:3000 --detach grafana/grafana-oss:latest
 docker-compose up --build --remove-orphans -d
